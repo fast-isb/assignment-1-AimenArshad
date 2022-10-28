3d1be1f3e8e6cefc8eac27cb833a74da19b0045d
shhdh <95531627+MuqadisaEjaz@users.noreply.github.com>
2022-10-28 05:00:34 +0500

    final

--- frontend/src/components/profileDisplay.jsx
+++ frontend/src/components/profileDisplay.jsx
@@ -1,7 +1,22 @@
 import { Card, Grid, Text, Button,useTheme, Row, Spacer} from "@nextui-org/react";
+import Axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Profile() {
+
+    const [resident,setResident] =useState(0);
+
+    useEffect(()=>{
+      (async ()=>{
+        Axios.get('http://localhost:5000/api/Resident/get/3600000000').then((response)=> {
+          setResident(response.data);
+          });})();},[]);
+
   return (
+
+    <div>
+    {(typeof resident.fname==='undefined' )? (<p>Loading Data...</p>):(
     <div>
      <h1>hello</h1>
           <Card css={{height:"auto",width:"auto",alignItems:"center"}}>
@@ -10,15 +25,17 @@ export default function Profile() {
           </Card.Header>
           <Card.Divider />
           <Card.Body >
-           <input type="text" id="fname" placeholder="First Name" disabled></input>
+           <input type="text" id="fname" placeholder={resident.fname} disabled></input>
            <Spacer y={0.5} />
-           <input type="text" id="lname" placeholder="Last Name" disabled></input>
+           <input type="text" id="lname" placeholder={resident.lname} disabled></input>
            <Spacer y={0.5} />
-           <input type="text" id="cnic" placeholder="CNIC" disabled></input>
+           <input type="text" id="cnic" placeholder={resident.cnic} disabled></input>
            <Spacer y={0.5} />
-           <input type="text" id="email" placeholder="email" disabled ></input>
+           <input type="text" id="email" placeholder={resident.email} disabled ></input>
            <Spacer y={0.5} />
-           <input type="text" id="age" placeholder="age" disabled></input>
+           <input type="text" id="age" placeholder={resident.age} disabled></input>
+           <Spacer y={0.5} />
+          
           </Card.Body>
           <Card.Divider />
           <Card.Footer>
@@ -29,6 +46,9 @@ export default function Profile() {
             </Row>
           </Card.Footer>
         </Card> 
+      </div>
+
+      )}
       </div>
   );
 }
--- frontend/src/components/updateProfile.jsx
+++ frontend/src/components/updateProfile.jsx
@@ -1,7 +1,56 @@
 import { Card, Grid, Text, Button,useTheme, Row, Spacer} from "@nextui-org/react";
+import Axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function UpdateProfile() {
+  const [resident,setResident] =useState(0);
+
+  const [fname,setfname] =useState(0);
+  const [lname,setlname] =useState(0);
+  const [email,setemail] =useState(0);
+  const [age,setage] =useState(0);
+  const [password,setpassword] =useState(0);
+
+
+
+    useEffect(()=>{
+      (async ()=>{
+        Axios.get('http://localhost:5000/api/Resident/get/3600000000').then((response)=> {
+          setResident(response.data);
+          
+          });})();},[]);
+
+     const updateUser=()=>{
+            Axios.put(`http://localhost:5000/api/Resident/update/${resident.cnic}`,{password:password,fname: fname,lname:lname,age:age,email:email});
+            
+         }
+
+       const fieldCheck=()=>{
+        if(fname===0){
+          setfname(resident.fname);
+
+        }
+        if(lname===0){
+          setlname(resident.lname);
+        }
+        if(email===0){
+          setemail(resident.email);
+        }
+        if(password===0){
+          setpassword(resident.password);
+        }
+        if(age===0){
+          setage(resident.age);
+        }
+        updateUser();
+       }
+
   return (
+
+
+    <div>
+    {(typeof resident.fname==='undefined' )? (<p>Loading Data...</p>):(
     <div>
      <h1>hello</h1>
           <Card css={{height:"auto",width:"auto",alignItems:"center"}}>
@@ -10,25 +59,41 @@ export default function UpdateProfile() {
           </Card.Header>
           <Card.Divider />
           <Card.Body >
-           <input type="text" id="fname" placeholder="First Name"></input>
+           <input type="text" id="fname"  defaultValue={resident.fname}      
+           onChange={(event)=> { setfname(event.target.value);}}   
+            ></input>
            <Spacer y={0.5} />
-           <input type="text" id="lname" placeholder="Last Name"></input>
+           <input type="text" id="lname" defaultValue={resident.lname}
+            onChange={(event)=> { setlname(event.target.value);}} 
+            ></input>
            <Spacer y={0.5} />
-           <input type="text" id="cnic" placeholder="CNIC" disabled></input>
+           <input type="text" id="cnic" placeholder={resident.cnic} disabled></input>
            <Spacer y={0.5} />
-           <input type="text" id="email" placeholder="email"></input>
+           <input type="text" id="email" defaultValue={resident.email} 
+            onChange={(event)=> { setemail(event.target.value);}} 
+            ></input>
            <Spacer y={0.5} />
-           <input type="text" id="age" placeholder="age"></input>
+           <input type="text" id="age" defaultValue={resident.age} 
+            onChange={(event)=> { setage(event.target.value);}} 
+            ></input>
+            <Spacer y={0.5} />
+           <input type="text" id="password" placeholder= "Password"
+            onChange={(event)=> { setpassword(event.target.value);}} 
+            ></input>
           </Card.Body>
           <Card.Divider />
           <Card.Footer>
             <Row justify="flex-end">
-            <Button auto color="secondary" rounded flat>
+            <Button auto color="secondary" rounded flat onClick={fieldCheck}>
               Save
             </Button>
             </Row>
           </Card.Footer>
         </Card> 
+
+        </div>
+
+      )}
       </div>
   );
 }
