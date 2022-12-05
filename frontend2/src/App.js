ab4b41e2d1b46dea2585c78868823881621d549d
shhdh <95518409+AimenArshad@users.noreply.github.com>
2022-12-06 01:49:58 +0500

    Containerization

---
+++ frontend2/src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import NavBar from './components/navbar';
+ import Profile from './components/profileDisplay';
+ import UpdateProfile from './components/updateProfile';
+ import Announcements from './components/announcements';
+
+ import {  BrowserRouter, Routes, Route} from 'react-router-dom'
+
+ function App() {
+   return (
+     
+       <>
+       <NavBar/>
+       <Routes>
+         <Route path='/announcements' element ={<Announcements/>} />
+         <Route path='/profile' element ={<Profile/>} />
+         <Route path='/updateProfile' element ={<UpdateProfile/>} />
+         <Route path='*' element={<h1>Welcome to HMS</h1>} />
+       </Routes >
+      </>
+     
+   );
+ }
+export default App;
+
+//import './App.css';
+// import NavBar from './components/NavBar.js'
+// import Announcements  from './components/Announcements';
+// import ManageProperty from './components/ManageProperty';
+// import Complaints from './components/Complaints';
+// import CustomerService from './components/CustomerService';
+// import Profile from './components/profile';
+// import Complaintstatus from './components/ComplaintStatus'
+
+
+// import {  BrowserRouter, Routes, Route} from 'react-router-dom'
+
+// function App() {
+//   return (
+//     <div >
+//       <BrowserRouter>
+//       <NavBar/>
+//       <Routes>
+//         <Route path='/announcements' element ={<Announcements/>} />
+//         <Route path='/ManageProperty' element ={<ManageProperty/>} />
+//         <Route path='/complaints' element ={<Complaints/>} />
+//         <Route path='/CustomerService' element ={<CustomerService/>} />
+//         <Route path='/profile' element ={<Profile/>} />
+//         <Route path='/ComplaintStatus' element ={<Complaintstatus/>} />
+//         <Route path='*' element={<h1>Muqadissa ka dimagh ni hai</h1>} />
+//       </Routes >
+//      </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ frontend2/src/components/navbar.jsx
@@ -0,0 +1,64 @@
+import { Navbar, Link, Text, Avatar, Dropdown } from "@nextui-org/react";
+import { Layout } from "./layout.jsx";
+import { AcmeLogo } from "./acmelogo.jsx";
+
+
+export default function  NavBar(){
+  return (
+    <Layout>
+      <Navbar isBordered variant="sticky">
+        <Navbar.Toggle showIn="xs" />
+        <Navbar.Brand
+          css={{
+            "@xs": {
+              w: "12%",
+            },
+          }}
+        >
+          <AcmeLogo />
+          <Text b color="inherit" hideIn="xs">
+            HSM
+          </Text>
+        </Navbar.Brand>
+        <Navbar.Content
+          enableCursorHighlight
+          activeColor="secondary"
+          hideIn="xs"
+          variant="highlight-rounded"
+        >
+          <Navbar.Link href="#/profile">My Profile </Navbar.Link>
+
+          <Navbar.Link href="#/updateProfile">Update Profile</Navbar.Link>
+          <Navbar.Link href="#/announcements">Announcements</Navbar.Link>
+          <Navbar.Link>Customer Service</Navbar.Link>
+        </Navbar.Content>
+        <Navbar.Content
+          css={{
+            "@xs": {
+              w: "12%",
+              jc: "flex-end",
+            },
+          }}
+        >
+          <Dropdown placement="bottom-right">
+            <Navbar.Item>
+              <Dropdown.Trigger>
+                <Avatar
+                  bordered
+                  as="button"
+                  color="secondary"
+                  size="md"
+                  src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                />
+              </Dropdown.Trigger>
+            </Navbar.Item>
+            <Dropdown.Menu color="secondary">
+               <Dropdown.Item >Profile</Dropdown.Item>
+               <Dropdown.Item withDivider color="error"> Log Out </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Navbar.Content>
+      </Navbar>
+    </Layout>
+  );
+}
